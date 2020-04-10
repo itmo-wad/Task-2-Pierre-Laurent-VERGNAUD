f8e71efe82bd13b678e238fb3d7b90c938fdad31
Pierre-Laurent Vergnaud <pierre-laurentvergnaud@MacBook-Pro-de-Pierre-Laurent.local>
2020-04-10 20:37:59 +0300

    add /index route

--- app/routes.py
+++ app/routes.py
@@ -3,16 +3,17 @@ from flask import Flask, send_from_directory, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/img/<path:filename>')
-def index2(filename):
-    return send_from_directory('img/', filename)
+@app.route('/img/<path:img>')
+def img(img):
+    return send_from_directory('img/', img)
 
-@app.route('/static/css/<string:stylesheet>')
-def index3(stylesheet):
-    return send_from_directory('static/css/', stylesheet)
+@app.route('/static/css/<string:css>')
+def css(css):
+    return send_from_directory('static/css/', css)
 
 if __name__ == "__main__":
     app.run(threaded=True, port='5000')
